fd285390330b1bd993377f84e94886946524ac2a
AlexHeustess <alexheustess@gmail.com>
2018-01-29 13:52:22 -0600

    Update FortuneTeller.java
    Working

--- FileReadingExample/src/FortuneTeller.java
+++ FileReadingExample/src/FortuneTeller.java
@@ -7,14 +7,13 @@ import java.io.*;
 public class FortuneTeller {
 
     public static void main(String[] args) {
-        File inputFile = new File("/answers.txt");
         ArrayList<String> answers = new ArrayList<String>();
-        String answer, response = "y";
+        String answer;
         Random rand = new Random();
         try {
+            File inputFile = new File("answers.txt");
             Scanner input = new Scanner(inputFile);
-            while(input.hasNextLine()) {
-                answer = input.nextLine();
+            while ((answer = input.nextLine())!= null) {
                 answers.add(answer);
             }
         } catch (Exception e) {
@@ -22,13 +21,15 @@ public class FortuneTeller {
             System.out.println(e.toString());
         }
         Scanner in = new Scanner(System.in);
-        while(response.equalsIgnoreCase("y")); {
+
+        String response = "y";
+        while(response.equalsIgnoreCase("y")) {
             System.out.println("The fortune teller is ready for you \n"
-                                + "Please think about your questions in your mind\n"
-                                + "and hit enter for your reply");
+                    + "Please think about your questions in your mind\n"
+                    + "and hit enter for your reply");
             in.nextLine();
             System.out.println("The fortune teller says: \""
-                                + answers.get(rand.nextInt(answers.size())) + "\"\n");
+                    + answers.get(rand.nextInt(answers.size())) + "\"\n");
             System.out.println("Do you have another question? (y/n)");
             response = in.nextLine();
         }
