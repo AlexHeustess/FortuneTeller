0f887d40ae8f79e70c0c5305c9319662cd5bd9b3
AlexHeustess <alexheustess@gmail.com>
2018-01-29 13:30:45 -0600

    Update FortuneTeller.java

--- FileReadingExample/src/FortuneTeller.java
+++ FileReadingExample/src/FortuneTeller.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class FortuneTeller {
 
     public static void main(String[] args) {
-        File inputFile = new File("answers.txt");
+        File inputFile = new File("/answers.txt");
         ArrayList<String> answers = new ArrayList<String>();
         String answer, response = "y";
         Random rand = new Random();
